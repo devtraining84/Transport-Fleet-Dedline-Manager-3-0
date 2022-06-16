12377af040ad436af638a4c2675b04606da1c795
devtraining84 <dev.training.84@gmail.com>
2022-06-16 23:35:14 +0200

    ....

--- fleet3/vehicles/views.py
+++ fleet3/vehicles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.views import View
