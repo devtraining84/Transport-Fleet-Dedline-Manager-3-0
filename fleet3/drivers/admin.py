94dd111e0df9c72b0b6e3d4fceff74efb2ace961
devtraining84 <dev.training.84@gmail.com>
2022-07-17 20:41:55 +0200

    add another models to admin

--- fleet3/drivers/admin.py
+++ fleet3/drivers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from drivers.models import DriversModel, DriverCertificatesModel
 # Register your models here.
+
+
+
+
+@admin.register(DriversModel)
+class DriversModel(admin.ModelAdmin):
+    pass
+
+
+
+
+@admin.register(DriverCertificatesModel)
+class DriverCertificatesModel(admin.ModelAdmin):
+    pass
