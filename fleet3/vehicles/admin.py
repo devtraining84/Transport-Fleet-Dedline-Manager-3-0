5ba7b5b1faccf47e5957fa9c910a7fa59c34a600
devtraining84 <dev.training.84@gmail.com>
2022-07-17 20:39:38 +0200

    add models to admin panel

--- fleet3/vehicles/admin.py
+++ fleet3/vehicles/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from vehicles.models import VehiclePermitsAndDedlinesModel, VehiclesModel
 # Register your models here.
+
+
+
+
+@admin.register(VehiclesModel)
+class VehiclesModel(admin.ModelAdmin):
+    pass
+
+
+
+
+
+@admin.register(VehiclePermitsAndDedlinesModel)
+class VehiclePermitsAndDedlinesModel(admin.ModelAdmin):
+    pass
+
+
+
+
